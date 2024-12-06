e9d81807c7fef4f962b62f3298f87c703bf16e62
Dennis Snell <dmsnell@xkq.io>
2024-12-06 01:30:00 -0700

    Find path to find loops.

--- src/day6.erl
+++ src/day6.erl
@@ -1,8 +1,12 @@
 -module(day6).
 -moduledoc """
 Started at 10:00pm
-Part 1 solved at 11:00pm [first attempt, 313, was not successful (min/max copy mistake)]
+Part 1 solved at 11:00pm
+    - first attempt, 313, was not successful (min/max copy mistake).
 Part 2 solved at…
+    - first attempt, 756, was not successful (pretty sure it would be wrong)
+    - second, 714, was wrong (thought it was because I had previously added
+      blockers where the path had already traversed, but this wasn’t the fix).
 Solved in…
 
 @author Dennis Snell <dmsnell@xkq.io>
@@ -23,7 +27,7 @@ config() -> #{
 p1_submitted(Lines) ->
     {ok, {_Rows, _Cols} = Size, Obstacles, Guard} = build_map(Lines),
     {_Exit, Traversed} = navigate(Size, Obstacles, Guard),
-    maps:size(Traversed).
+    maps:size(Traversed) - 3.
 
 
 build_map(Lines) ->
@@ -48,65 +52,71 @@ parse_line(Obstacles, Guard, Row, Col, <<_, Line/binary>>) ->
 
 
 navigate({_H, _W} = Size, Obstacles, {Row, Col, _Dir} = Guard) ->
-    navigate(Size, Obstacles, Guard, log_new({Row, Col}, up)).
+    navigate(Size, Obstacles, Guard, log_new(Obstacles, Size, {Row, Col}, up)).
 
 navigate({H, W} = Size, Obstacles, {Row, Col, Dir} = _Guard, Traversed) ->
     From = {Row, Col},
-    case Dir of
-        up ->
-            case [R || {R, C} := _ <- Obstacles, C == Col, R < Row] of
-                [] ->
-                    {{1, Col}, traverse(Traversed, Dir, From, {1, Col})};
-                InWay ->
-                    R = lists:max(InWay),
-                    navigate(
-                        Size,
-                        Obstacles,
-                        {R + 1, Col, turn(Dir)},
-                        traverse(Traversed, Dir, From, {R + 1, Col})
-                    )
-            end;
+    case Traversed of
+        #{{Row, Col} := #{Dir := true}} ->
+            loop;
 
-        down ->
-            case [R || {R, C} := _ <- Obstacles, C == Col, R > Row] of
-                [] ->
-                    {{H, Col}, traverse(Traversed, Dir, From, {H, Col})};
-                InWay ->
-                    R = lists:min(InWay),
-                    navigate(
-                        Size,
-                        Obstacles,
-                        {R - 1, Col, turn(Dir)},
-                        traverse(Traversed, Dir, From, {R - 1, Col})
-                    )
-            end;
+        _ ->
+            case Dir of
+                up ->
+                    case [R || {R, C} := _ <- Obstacles, C == Col, R < Row] of
+                        [] ->
+                            {{1, Col}, traverse(Traversed, Dir, From, {1, Col})};
+                        InWay ->
+                            R = lists:max(InWay),
+                            navigate(
+                                Size,
+                                Obstacles,
+                                {R + 1, Col, turn(Dir)},
+                                traverse(Traversed, Dir, From, {R + 1, Col})
+                            )
+                    end;
 
-        left ->
-            case [C || {R, C} := _ <- Obstacles, R == Row, C < Col] of
-                [] ->
-                    {{Row, 1}, traverse(Traversed, Dir, From, {Row, 1})};
-                InWay ->
-                    C = lists:max(InWay),
-                    navigate(
-                        Size,
-                        Obstacles,
-                        {Row, C + 1, turn(Dir)},
-                        traverse(Traversed, Dir, From, {Row, C + 1})
-                    )
-            end;
+                down ->
+                    case [R || {R, C} := _ <- Obstacles, C == Col, R > Row] of
+                        [] ->
+                            {{H, Col}, traverse(Traversed, Dir, From, {H, Col})};
+                        InWay ->
+                            R = lists:min(InWay),
+                            navigate(
+                                Size,
+                                Obstacles,
+                                {R - 1, Col, turn(Dir)},
+                                traverse(Traversed, Dir, From, {R - 1, Col})
+                            )
+                    end;
 
-        right ->
-            case [C || {R, C} := _ <- Obstacles, R == Row, C > Col] of
-                [] ->
-                    {{Row, W}, traverse(Traversed, Dir, From, {Row, W})};
-                InWay ->
-                    C = lists:min(InWay),
-                    navigate(
-                        Size,
-                        Obstacles,
-                        {Row, C - 1, turn(Dir)},
-                        traverse(Traversed, Dir, From, {Row, C - 1})
-                    )
+                left ->
+                    case [C || {R, C} := _ <- Obstacles, R == Row, C < Col] of
+                        [] ->
+                            {{Row, 1}, traverse(Traversed, Dir, From, {Row, 1})};
+                        InWay ->
+                            C = lists:max(InWay),
+                            navigate(
+                                Size,
+                                Obstacles,
+                                {Row, C + 1, turn(Dir)},
+                                traverse(Traversed, Dir, From, {Row, C + 1})
+                            )
+                    end;
+
+                right ->
+                    case [C || {R, C} := _ <- Obstacles, R == Row, C > Col] of
+                        [] ->
+                            {{Row, W}, traverse(Traversed, Dir, From, {Row, W})};
+                        InWay ->
+                            C = lists:min(InWay),
+                            navigate(
+                                Size,
+                                Obstacles,
+                                {Row, C - 1, turn(Dir)},
+                                traverse(Traversed, Dir, From, {Row, C - 1})
+                            )
+                    end
             end
     end.
 
@@ -126,26 +136,84 @@ traverse(Seen, Dir, _Delta, To, To) ->
 traverse(Seen, Dir, Delta, From, To) ->
     traverse(log(Seen, From, Dir), Dir, Delta, step(Delta, From), To).
 
-log_new({R, C} = _At, Dir) ->
-    #{{R, C} => #{Dir => true}}.
+log_new(Obstacles, Size, {_R, _C} = _At, _Dir) ->
+    #{
+%%        {R, C} => #{Dir => true},
+        size => Size,
+        loops => #{},
+        obstacles => Obstacles
+    }.
 
 log(Seen, At, Dir) ->
     maps:update_with(
         At,
         fun (Dirs) -> Dirs#{Dir => true} end,
         #{Dir => true},
-        Seen
+        find_blocker(Seen, At, Dir)
     ).
 
+find_blocker(#{loops := Loops, size := Size, obstacles := Obstacles} = Seen, At, Dir) ->
+    NeedDir = turn(Dir),
+    case is_map_key(step(NeedDir, At), Obstacles) of
+        true ->
+            Seen;
 
-delta(up) -> {-1, 0};
-delta(right) -> {0, 1};
-delta(down) -> {1, 0};
-delta(left) -> {0, -1}.
+        false ->
+            case find_first_path(Obstacles, Seen, Size, NeedDir, At, At, #{}) of
+                {ok, _Looper} ->
+                    BlockerAt = step(Dir, At),
+                    % Can’t install an obstacle where the path has already crossed.
+                    case maps:get(BlockerAt, Seen, free) of
+                        free ->
+                            Seen#{loops => Loops#{step(Dir, At) => true}};
+
+                        _ ->
+                            Seen
+                    end;
+
+                not_found ->
+                    Seen
+            end
+    end.
+
+
+find_first_path(_Obstacles, _Seen, {W, H}, _Dir, {R, C}, _Origin, _Visited) when R < 1; C < 1; R > H; C > W ->
+    not_found;
+find_first_path(Obstacles, Seen, Size, Dir, At, Prev, Visited) ->
+    SeenAt = maps:get(At, Seen, not_found),
+    ObstacleAt = maps:get(At, Obstacles, not_found),
+    case {ObstacleAt, SeenAt, is_map_key(At, Visited)} of
+        {not_found, #{Dir := true}, _} ->
+            {ok, At};
+
+        {_, _, true} ->
+            {ok, At};
+
+        % Turn at an obstacle (could not have visited an obstacle).
+        {true, not_found, false} ->
+            NewDir = turn(Dir),
+            Reflect = turn(NewDir),
+            case step(Reflect, At) of
+                % Can’t reflect back at self.
+                Prev ->
+                    not_found;
+
+                Before ->
+                    find_first_path(Obstacles, Seen, Size, NewDir, step(NewDir, Before), Before, Visited#{At => true})
+            end;
+
+        {not_found, _, _} ->
+            find_first_path(Obstacles, Seen, Size, Dir, step(Dir, At), At, Visited#{At => true})
+    end.
 
 delta(From, From) -> 0;
 delta(From, To) when From < To -> 1;
 delta(From, To) when From > To -> -1.
+
+step(up, {R, C}) -> {R - 1, C};
+step(down, {R, C}) -> {R + 1, C};
+step(left, {R, C}) -> {R, C - 1};
+step(right, {R, C}) -> {R, C + 1};
 
 step({DR, DC} = _Delta, {R, C} = _From) ->
     {R + DR, C + DC}.
@@ -161,7 +229,7 @@ p2_submitted(Lines) ->
     {ok, {_Rows, _Cols} = Size, Obstacles, {Row, Col, _} = Guard} = build_map(Lines),
     {_Exit, #{loops := Loops}} = navigate(Size, Obstacles, Guard),
     Blockers = maps:remove({Row, Col}, Loops),
-    Blockers.
+    maps:size(Blockers).
 
 
 -ifdef(EUNIT).
@@ -170,7 +238,54 @@ p2_submitted(Lines) ->
 p1_test() ->
     ?assertEqual(41, aoc:test(day6, p1, "day6_a")).
 
-p2_test() ->
-    ?assertEqual(6, aoc:test(day6, p2, "day6_a")).
+p2_test_() -> [
+    {"Example", ?_assertEqual(6, aoc:test(day6, p2, "day6_a"))},
+    {
+        """
+        Blockers not only appear when crossing a path, but also when
+        passing a point that if turned, would re-join a prior path.
+        """,
+        ?_assertNotEqual(756, aoc:test(day6, p2, "day6"))
+    },
+    {
+        """
+        Can’t place blockers on a spot already traversed, else the
+        guard wouldn’t be able to arrive at the current spot.
+        """,
+        ?_assertNotEqual(714, aoc:test(day6, p2, "day6"))
+    },
+    {
+        """
+        If finding a path and an obstacle is in the way, one needs
+        to turn before the obstacle and recurse, continue looking
+        for a path that would connect to a loop.
+        """,
+        ?_assertNotEqual(428, aoc:test(day6, p2, "day6"))
+    },
+    {
+        """
+        Not sure why this is wrong.
+        """,
+        ?_assertNotEqual(1338, aoc:test(day6, p2, "day6"))
+    },
+    {
+        """
+        Will this be the one?
+        """,
+        ?_assertNotEqual(1500, aoc:test(day6, p2, "day6"))
+    },
+    {
+        """
+        Didn’t get the visitation check right, because I was
+        checking if the loop-finder had already visited obstacles,
+        which isn’t possible; it should have been checking that
+        it wasn’t re-visiting locations that didn’t also have
+        a previously-seen traversal.
+
+        But this is also wrong :(
+        """,
+        ?_assertNotEqual(1339, aoc:test(day6, p2, "day6"))
+    }
+].
 
 -endif.
